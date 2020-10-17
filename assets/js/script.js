5ec40cceb5c65205e46eecece915171b42d86750
Drake Stephens <meuio56@gmail.com>
2020-10-16 20:24:36 -0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,40 @@
+var generateBtn = document.getElementById('generate');
 // Assignment code here
+var slider = document.getElementById("myRange");
+const val = slider.value
+const output = document.getElementById("demo");
+const current = output.innerHTML = val; // Display the default slider value
+const password = document.getElementById('password');
+
+const string = "abcdefghijklmnopqrstuvwxyz`~1234567890!@#$%^&*()_+=-|";
+
+
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function wPassword(l, characters){
+  
+  var pwd = '';
 
-  passwordText.value = password;
-
+  for(var i = 0; i<l; i++){
+      pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password.innerHTML = pwd;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function(){
+  password.innerHTML = "";
+  console.log('hi')
+  console.log(wPassword(slider.value, string))
+  wPassword(slider.value, string)
+});
